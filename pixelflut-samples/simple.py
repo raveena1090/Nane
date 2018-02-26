5625eeba09eb9e382aa43bfb9d4acb34c28a59f9
Raveena Sawhney <Raveena.Sawhney@vodafone.com>
2018-02-26 18:44:25 +0000

    Updated code to add encoding

--- pixelflut-samples/simple.py
+++ pixelflut-samples/simple.py
@@ -4,8 +4,9 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('127.0.0.1', 1234))
 
-def px(x, y, rgba='ffffffff'):
-	sock.send('PX {} {} {}\n'.format(x, y, rgba))
+def px(x, y, rgba="ffffffff"):
+    data = "PX {} {} {}\n".format(x, y, rgba).encode('ascii')
+    sock.send(data)
 
 px(23, 42)
 
